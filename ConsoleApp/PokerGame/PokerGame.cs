f68cbb1ee2011d4fae0113d973276e5fb8ec3275
Otávio Augusto De Queiroz Reis <1147990@sga.pucminas.br>
2019-02-21 21:15:59 -0300

    Inclusão do teste IsRoyalFlush

--- ConsoleApp/PokerGame/PokerGame.cs
+++ ConsoleApp/PokerGame/PokerGame.cs
@@ -14,36 +14,62 @@ namespace ConsoleApp
         public List<Carta> MaoJogadorUm { get; set; }
         public List<Carta> MaoJogadorDois { get; set; }
 
+        public bool IsRoyalFlush(List<Carta> maoJogador)
+        {
+            var maoJogadorOrdenada = maoJogador.OrderBy(v => v.Valor).ToList();
+            var isRoyalFlush = maoJogadorOrdenada[0].Valor == Valor.As &&
+                               maoJogadorOrdenada[1].Valor == Valor.Dez &&
+                               maoJogadorOrdenada[2].Valor == Valor.Valete &&
+                               maoJogadorOrdenada[3].Valor == Valor.Dama &&
+                               maoJogadorOrdenada[4].Valor == Valor.Rei;
 
+            return isRoyalFlush;
+        }
 
-
-
-        public bool IsFlush(List<Carta> maoJogador)
+        public bool IsStraightFlush(List<Carta> maoJogador)
         {
             throw new NotImplementedException();
         }
+
+
+        public bool IsQuadra(List<Carta> maoJogador)
+        {
+            bool isQuadra = false;
+
+            for (int i = 0; i < maoJogador.Count; i++)
+            {
+                for (int j = i + 1; j < maoJogador.Count; j++)
+                {
+                    for (int k = j + 1; k < maoJogador.Count; k++)
+                    {
+                        for (int w = k + 1; w < maoJogador.Count; w++)
+                        {
+                            if (maoJogador[i].Valor == maoJogador[j].Valor &&
+                                maoJogador[i].Valor == maoJogador[k].Valor &&
+                                maoJogador[i].Valor == maoJogador[w].Valor)
+                            {
+                                isQuadra = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return isQuadra;
+        }
+
 
         public bool IsFullHouse(List<Carta> maoJogador)
         {
             throw new NotImplementedException();
         }
 
-        public bool IsQuadra(List<Carta> maoJogador)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsRoyalFlush(List<Carta> maoJogador)
+        public bool IsFlush(List<Carta> maoJogador)
         {
             throw new NotImplementedException();
         }
 
         public bool IsStraight(List<Carta> maoJogador)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsStraightFlush(List<Carta> maoJogador)
         {
             throw new NotImplementedException();
         }
@@ -59,7 +85,7 @@ namespace ConsoleApp
                     for (int k = j + 1; k < maoJogador.Count; k++)
                     {
                         if (maoJogador[i].Valor == maoJogador[j].Valor &&
-                            maoJogador[i].Valor == maoJogador[k].Valor)
+                            maoJogador[i].Valor == maoJogador[k].Valor && !IsQuadra(maoJogador))
                         {
                             isTrinca = true;
                         }
@@ -120,10 +146,13 @@ namespace ConsoleApp
             Console.WriteLine("isUmPar: " + IsUmPar(MaoJogadorUm));
             Console.WriteLine("isDoisPares: " + IsDoisPares(MaoJogadorUm));
             Console.WriteLine("IsTrinca: " + IsTrinca(MaoJogadorUm));
+            Console.WriteLine("IsQuadra: " + IsQuadra(MaoJogadorUm));
             Console.WriteLine("----------");
             Console.WriteLine("isUmPar: " + IsUmPar(MaoJogadorDois));
             Console.WriteLine("isDoisPares: " + IsDoisPares(MaoJogadorDois));
             Console.WriteLine("IsTrinca: " + IsTrinca(MaoJogadorDois));
+            Console.WriteLine("IsQuadra: " + IsQuadra(MaoJogadorDois));
+
 
             return "";
         }
